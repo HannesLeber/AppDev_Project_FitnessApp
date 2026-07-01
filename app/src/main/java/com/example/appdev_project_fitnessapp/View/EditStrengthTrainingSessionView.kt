ab3b08ddf56c6919e43cf985a93a2185174ba5a4
HannesLeber <hannes.leber@hotmail.com>
2026-07-01 22:18:37 +0200

    Added new empty Views

---
+++ app/src/main/java/com/example/appdev_project_fitnessapp/View/EditStrengthTrainingSessionView.kt
@@ -0,0 +1,11 @@
+package com.example.appdev_project_fitnessapp.View
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import com.example.appdev_project_fitnessapp.ViewModel.StrengthTrainingViewModel
+
+@Composable
+fun EditStrengthTrainingSessionView(navController: NavHostController, strengthTrainingViewModel: StrengthTrainingViewModel) {
+
+}
