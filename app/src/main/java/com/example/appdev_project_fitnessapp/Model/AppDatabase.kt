43de23173db64536f890f2493db97a54287c7d5e
HannesLeber <hannes.leber@hotmail.com>
2026-07-01 22:16:48 +0200

    Added update Version of DB

--- app/src/main/java/com/example/appdev_project_fitnessapp/Model/AppDatabase.kt
+++ app/src/main/java/com/example/appdev_project_fitnessapp/Model/AppDatabase.kt
@@ -6,7 +6,9 @@ import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-@Database(entities = [TrainingSession::class, DoneExercise::class, Exercise::class, ExerciseSet::class], version = 1)
+@Database(entities = [TrainingSession::class, DoneExercise::class, Exercise::class, ExerciseSet::class],
+    version = 2,
+    exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun trainingSessionDao(): TrainingSessionDao
