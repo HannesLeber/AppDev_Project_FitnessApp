0b3fc3b6ba12f0a4870fe8085145334821c87cf6
HannesLeber <hannes.leber@hotmail.com>
2026-07-01 22:18:19 +0200

    Added ordering and TrainingTemplateDAO

--- app/src/main/java/com/example/appdev_project_fitnessapp/Model/StrengthTrainingDAOs.kt
+++ app/src/main/java/com/example/appdev_project_fitnessapp/Model/StrengthTrainingDAOs.kt
@@ -3,12 +3,14 @@ package com.example.appdev_project_fitnessapp.Model
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import java.util.Date
 
+//TODO: Update-Funktionen schreiben! Aber nicht löschen und dann neu hinzufügen, sonst Problem mit den IDs
 @Dao
 interface TrainingSessionDao {
-    @Query("SELECT * FROM TrainingSession")
+    @Query("SELECT * FROM TrainingSession ORDER BY id DESC")
     suspend fun getAll(): List<TrainingSession>
 
     @Query("SELECT * FROM TrainingSession WHERE id IN (:trainingSessionIds)")
@@ -35,10 +37,10 @@ interface TrainingSessionDao {
 
 @Dao
 interface DoneExerciseDao {
-    @Query("SELECT * FROM DoneExercise")
+    @Query("SELECT * FROM DoneExercise ORDER BY id DESC")
     suspend fun getAll(): List<DoneExercise>
 
-    @Query("SELECT * FROM DoneExercise WHERE id IN (:doneExerciseIds)")
+    @Query("SELECT * FROM DoneExercise WHERE id IN (:doneExerciseIds) ")
     suspend fun loadAllByIds(doneExerciseIds: IntArray): List<DoneExercise>
 
     @Query("SELECT * FROM DoneExercise WHERE id = :id")
@@ -56,7 +58,7 @@ interface DoneExerciseDao {
 
 @Dao
 interface ExerciseDao {
-    @Query("SELECT * FROM Exercise")
+    @Query("SELECT * FROM Exercise ORDER BY id DESC")
     suspend fun getAll(): List<Exercise>
 
     @Query("SELECT * FROM Exercise WHERE id IN (:exerciseIds)")
@@ -97,4 +99,17 @@ interface SetDao {
 
     @Delete
     suspend fun delete(set: ExerciseSet)
+}
+
+
+@Dao
+interface TrainingTemplateDao {
+    @Query("SELECT * FROM TrainingTemplate")
+    suspend fun getAll(): List<TrainingTemplate>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(template: TrainingTemplate): Long
+
+    @Delete
+    suspend fun delete(template: TrainingTemplate)
 }
