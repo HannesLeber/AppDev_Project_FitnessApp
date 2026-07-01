514985a11f32e5d0172e122dca24fac1e4ceb545
HannesLeber <hannes.leber@hotmail.com>
2026-07-01 22:15:20 +0200

    created ViewModel in upper Composable, to only create it once and pass it down

--- app/src/main/java/com/example/appdev_project_fitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/appdev_project_fitnessapp/MainActivity.kt
@@ -8,12 +8,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.appdev_project_fitnessapp.View.HomeScreen
 import com.example.appdev_project_fitnessapp.View.StrengthTrainingView
+import com.example.appdev_project_fitnessapp.View.ChooseTemplateView
+import com.example.appdev_project_fitnessapp.View.EditStrengthTrainingSessionView
+import com.example.appdev_project_fitnessapp.ViewModel.StrengthTrainingViewModel
 import com.example.appdev_project_fitnessapp.ui.theme.AppDev_Project_FitnessAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,12 +36,14 @@ class MainActivity : ComponentActivity() {
 
 
 @Composable
-fun AppNavigation(){
+fun AppNavigation(strengthTrainingViewModel: StrengthTrainingViewModel = viewModel(factory = StrengthTrainingViewModel.Factory)){
     val navController = rememberNavController()
 
     NavHost(navController, startDestination = "home") {
         //TODO: add screens
         composable("home") { HomeScreen(navController) }
-        composable("strengthTraining") { StrengthTrainingView(navController) }
+        composable("strengthTraining") { StrengthTrainingView(navController, strengthTrainingViewModel) }
+        composable("chooseTemplate") { ChooseTemplateView(navController, strengthTrainingViewModel) }
+        composable("editStrengthTraining") { EditStrengthTrainingSessionView(navController, strengthTrainingViewModel) }
     }
 }
