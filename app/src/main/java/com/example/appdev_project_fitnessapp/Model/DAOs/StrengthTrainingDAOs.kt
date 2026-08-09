f761ec6f65d038cb020c9f1475b8011aea127a48
HannesLeber <hannes.leber@hotmail.com>
2026-08-09 20:35:10 +0200

    Added functionality to save PR-Sets and display the current PR-Set of an exercise on the Top of the EditDoneExerciseView

--- app/src/main/java/com/example/appdev_project_fitnessapp/Model/DAOs/StrengthTrainingDAOs.kt
+++ app/src/main/java/com/example/appdev_project_fitnessapp/Model/DAOs/StrengthTrainingDAOs.kt
@@ -54,6 +54,9 @@ interface DoneExerciseDao {
     @Query("SELECT * FROM DoneExercise WHERE id = :id")
     suspend fun findById(id: Int): DoneExercise
 
+    @Query("SELECT * FROM DoneExercise WHERE exerciseID = :exerciseID")
+    suspend fun findByExerciseID(exerciseID: Int): List<DoneExercise>
+
     @Insert
     suspend fun insert(doneExercise: DoneExercise): Long
 
@@ -89,6 +92,9 @@ interface ExerciseDao {
 
     @Delete
     suspend fun delete(exercise: Exercise)
+
+    @Query("UPDATE Exercise SET prSetID = :newPrSetID WHERE id = :id")
+    suspend fun updatePrSetID(id: Int, newPrSetID: Int?)
 }
 
 @Dao
